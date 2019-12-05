de1fdf28b0920f2a27929dbd3dd11442602539b5
ZulianTiger <ady0606@gmail.com>
2019-12-05 19:14:46 +0100

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './Containers/Home'
+import About from './Containers/About'
+import Contact from './Containers/Contact'
+import NoPage from './Containers/NoPage'
+import { Layout } from './Components/Layout'
+import { NavigationBar } from './Components/NavigationBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoPage} />
+          </Switch>
+        </Layout>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/Containers/NoPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NoPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>NoPage page</h2>
+            </div>
+        )
+    }
+}
+
+export default NoPage;
