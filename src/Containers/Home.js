f9ce4c78198a67511676080f6488398f5db139cf
ZulianTiger <ady0606@gmail.com>
2019-12-10 22:34:33 +0100

    Added landing bg image

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import HomeTitle from '../Components/HomeTitle'
 import NewButton from '../Components/NewButton'
@@ -6,18 +7,26 @@ import { Layout } from '../Components/Layout'
 import { NavigationBar } from '../Components/NavigationBar'
 import { Section } from "react-scroll-section";
 
+import landingimg from '../Images/landingbg.png'
+
+const LandingBg = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background-image: url(${landingimg});
+`
+
 class Home extends Component {
     render() {
         return (
             <Section id="home">
-                <div style={{ height: "100vh", backgroundImage: "url(https://images.pexels.com/photos/333850/pexels-photo-333850.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)" }}>
+                <LandingBg>
                     <NavigationBar />
                     <Layout>
                         <HomeTitle>This is a styled component header 1.</HomeTitle>
                         <NewButton primary slide text="Primary" containerWidth="30%" />
                         <NewButton text="Secondary" containerWidth="30%" />
                     </Layout>
-                </div>
+                </LandingBg>
             </Section>
 
         )
