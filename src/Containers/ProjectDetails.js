36e4b97a0f8e21c094885695fce8c0edab2fdae1
ZulianTiger <ady0606@gmail.com>
2019-12-23 23:14:43 +0100

    Passing nonexistent props to projects search will now show 404 page

--- src/Containers/ProjectDetails.js
+++ src/Containers/ProjectDetails.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import history from '../Components/history'
+
+let projectID;
 
 export default class ProjectDetails extends Component {
+
+    componentWillMount() {
+        projectID = this.props.location.search;
+        this.checkProjectID(projectID);
+    }
+
+    checkProjectID (ID) {
+        if (ID != "?id=0" && ID != "?id=1" && ID != "?id=2"){
+            history.push('404');
+            window.location.reload();
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>Project ID: {this.props.location.search}</h1>
+                <h1>Project ID: {projectID}</h1>
                 <h2>Project details page</h2>
                 <h2>this.props {JSON.stringify(this.props)}</h2>
             </div>
